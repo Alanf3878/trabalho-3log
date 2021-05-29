ad09ecb1d88a620a153a1bf887b04448bdfbbb0f
Alan Fernandes <alanf3878@outlook.com>
2021-05-29 08:22:36 -0300

    trabalho3]

---
+++ java.js
@@ -0,0 +1,22 @@
+ for (let numeros = 1; numeros <= 10; numeros ++) {
+    console.log (numeros)
+}
+
+
+
+conta = 1
+
+while (conta <= 10) {
+    console.log (conta)
+    conta ++
+}
+
+
+for (let par = 0; par <= 100; par +=2){
+    console.log(par)
+}
+
+for (let primo = 1; primo <= 100; primo +=2){
+    console.log(primo)
+}
+
